81e527c1630e2f2fdbef2cb9dcb68014c6f34a39
Udoka <udochukwukaonyela@gmail.com>
2022-09-16 10:10:44 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Card from "./Card";
 import Home from "./pages/Home";
-const data = [{
-  firstName: "john",
-  lastName: "doe",
-  Date: Date.now(),
-  userName: "MrJohn"
-  },
-  {
-  firstName: "james",
-  lastName: "doe",
-  Date: Date.now(),
-  userName: "MrJohn"
-  },
-  {
-  firstName: "martins",
-  lastName: "doe",
-  Date: Date.now(),
-  userName: "MrJohn"
-  },
-  {
-  firstName: "hshdj",
-  lastName: "doe",
-  Date: Date.now(),
-  userName: "MrJohn"
-  }
-];
-
 const App = () => {
   
   return (
