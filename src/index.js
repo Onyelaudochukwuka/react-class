b407d714784a4ede642d3c843a7f721e5f307eae
Udoka <udochukwukaonyela@gmail.com>
2022-09-28 10:54:56 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './Calculator';
+import Scroll from "./components/Scroll/Scroll";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <Scroll />
   </React.StrictMode>
 );
 
